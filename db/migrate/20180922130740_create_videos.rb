a42b3ea5a719b69bad09223c0ab2cd855f61cbff
Sylvance <kerandisylvance@gmail.com>
2018-09-22 16:07:55 +0300

    Add video resources

---
+++ db/migrate/20180922130740_create_videos.rb
@@ -0,0 +1,12 @@
+class CreateVideos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :videos do |t|
+      t.references :user, foreign_key: true
+      t.string :title
+      t.string :type
+      t.string :view_code
+
+      t.timestamps
+    end
+  end
+end
