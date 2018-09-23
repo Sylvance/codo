a60f97765a9b22d32ff863bdd27186bcd902e17d
Sylvance <kerandisylvance@gmail.com>
2018-09-23 21:38:26 +0300

    Fix user-reply relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :videos, dependent: :destroy
+  has_many :replies, dependent: :destroy
 end
